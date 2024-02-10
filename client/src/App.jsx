13d19acff9b8cf5f8e97f93ee003ee7f848dd179
niraj070707 <karandeniraj28@gmail.com>
2024-02-10 23:05:09 +0530

    Create-Real-Estate React-app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import RegisterAndLogin from './RegisterAndLogin'
+
+function App() {
+  return (
+    <div>
+      <RegisterAndLogin/>
+    </div>      
+  )
+}
+
+export default App
---
+++ client/src/RegisterAndLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RegisterAndLogin = () => {
+  return (
+    <div>RegisterAndLogin</div>
+  )
+}
+
+export default RegisterAndLogin
