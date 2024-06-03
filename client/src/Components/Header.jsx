9652a18514535aa237d1ebf234d07ab22df5068d
niraj070707 <karandeniraj28@gmail.com>
2024-06-03 12:23:45 +0530

    Create signin page and Add its functionality

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <header>
-            <div>
-                <h1 className=" bg-black w-80">
-                    Anand
-                </h1>
-                <h1>
-                    Estate
-                </h1>
-                <form className=" bg-slate-200">
+        <header className=" bg-slate-200 shadow-md">
+            <div className=" flex items-center justify-between max-w-6xl mx-auto p-3">
+                <Link to={'/'}>
+                    <h1 className=" flex font-bold text-sm sm:text-xl flex-wrap cursor-pointer">
+                        <span className=" text-slate-500">Anand</span>
+                        <span className=" text-slate-700">Estate</span>
+                    </h1>
+                </Link>
+                
+                <form className=" flex items-center bg-slate-100 p-3 rounded-lg">
                     <input 
                         type="text" 
                         placeholder="Search.." 
-                        className=" bg-transparent focus:outline-none w-80"
+                        className=" bg-transparent focus:outline-none w-32 sm:w-64"
                     />
+                    <FaSearch className=" text-slate-500" />
                 </form>
+                <ul className=" flex gap-4 sm:gap-10">
+                    <Link to={'/signin'}><li className=" sm:inline hover:underline cursor-pointer">Sign-in</li></Link>
+                    <Link to={'/about'}><li className=" hidden sm:inline hover:underline cursor-pointer">About</li></Link>
+                    <Link to={'/'}><li className=" hidden sm:inline hover:underline cursor-pointer">Home</li></Link>
+                </ul>
             </div>
         </header>
     );
--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signin = () => {
-  return (
-    <div>Signin</div>
-  )
-}
+    const [formdata, setFormData] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+    
+    const handleChange = (ev)=>{
+        setFormData({
+            ...formdata,
+            [ev.target.id] : ev.target.value,
+        })
+    }
+    console.log(formdata);
 
-export default Signin
+    const handleSubmit = async (ev)=>{
+        ev.preventDefault();
+        try{
+            setLoading(true);
+            const result = await fetch('/api/auth/signin', {
+                method : 'POST',
+                headers : {
+                    'Content-Type' : 'application/json',
+                }, 
+                body : JSON.stringify(formdata),
+            });
+    
+            const data = await result.json();
+            console.log(data);
+            if(data.success === false){
+                setLoading(false);
+                setError(data.message);
+                return;
+            }
+            setLoading(false);
+            setError(null);
+            navigate('/');
+        }catch(err){
+            setLoading(false);
+            setError(err.message);
+        } 
+    }
+
+    return (
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                <input
+                    type="email"
+                    placeholder="email"
+                    className="border p-3 rounded-lg"
+                    id="email"
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    className="border p-3 rounded-lg"
+                    id="password"
+                    onChange={handleChange}
+                />
+
+                <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+                    {loading ? "Loading..." : "Sign-In"}
+                </button>
+
+            </form>
+            <div className="flex gap-2 mt-5">
+                <p>Dont have an account?</p>
+                <Link to={"/signup"}>
+                    <span className="text-blue-700">Sign up</span>
+                </Link>
+            </div>
+            {error && <p className="text-red-500 mt-5">{error}</p>}
+        </div>
+    );
+};
+
+export default Signin;
